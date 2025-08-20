6dfd02be800221f51476c41c43c3eb1c86c4b378
rexfernandezluciano <134561258+rexfernandezluciano@users.noreply.github.com>
2025-08-20 12:21:46 +0000

    Attempting to fix CORS #1

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // CORS configuration to allow all requests from any host
 app.use(
 	cors({
-		origin: ["https://dopp.eu.org", "https://www.dopp.eu.org"],
+		origin: "*",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 		allowedHeaders: [
